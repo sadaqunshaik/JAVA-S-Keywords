9f44d0dc54749b06826eececa03ea2b48fd69d45
Your Name <sadaqunps@gmail.com>
2024-05-29 17:26:53 +0530

    keywords

---
+++ data.js
@@ -0,0 +1,23 @@
+var eid=101
+
+function emp_Age(){}
+
+class Employee{
+
+}
+
+let emp={ }
+
+
+//let typeof="RahulGandhi"
+//let this9 =100
+//let th!s =100
+//let super="Super Start"
+
+let constuctor="thiaba"
+console.log(constuctor)
+
+
+/*function instanceof(){
+    
+}*/
